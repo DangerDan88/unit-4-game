7f98a8e75acee133793e64cfa53746c41db2a410
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-09-12 14:58:31 -0600

    final commit for dragonball collecter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,42 @@
-/// Need to set up all your variables for game like score to reach wins, losses, playerscore, and random crystal numbers
-///
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     var wins = 0;
-var losses = 0;
-var gameScore = (Math.floor(Math.random() * 130 - 22) + 22);
-var crystalOne = Math.floor(Math.random() * 12) + 1
-var crystalTwo = Math.floor(Math.random() * 12) + 1;
-var crystalThree = Math.floor(Math.random() * 12) + 1;
-var crystalFour = Math.floor(Math.random() * 12) + 1;
-var playerScore = 0;
+    var losses = 0;
+    var gameScore = (Math.floor(Math.random() * 130 - 22) + 22);
+    var crystalOne = Math.floor(Math.random() * 12) + 1
+    var crystalTwo = Math.floor(Math.random() * 12) + 1;
+    var crystalThree = Math.floor(Math.random() * 12) + 1;
+    var crystalFour = Math.floor(Math.random() * 12) + 1;
+    var playerScore = 0;
 
     $("#gameScore").text(gameScore);
-    $('#crystalOne').data("value",crystalOne);
-    $('#crystalTwo').data("value",crystalTwo);
-    $('#crystalThree').data("value",crystalThree);
-    $('#crystalFour').data("value",crystalFour);
-    $(".crystal").click(function(event) {
+    $('#crystalOne').data("value", crystalOne);
+    $('#crystalTwo').data("value", crystalTwo);
+    $('#crystalThree').data("value", crystalThree);
+    $('#crystalFour').data("value", crystalFour);
+    $(".crystal").click(function (event) {
         console.log($(event.target).data().value);
 
-    var crystalValue = parseInt($(event.target).data().value);
-    playerScore = playerScore + crystalValue;
-    $("#playerScore").text(playerScore)
-    $("#gameScore").text(gameScore)
-    
-    console.log(playerScore);
-    if(playerScore === gameScore) {
-        wins++
-        alert("you win");
-    }
-    else if (playerScore > gameScore) {
-        losses++
-       alert("You suck");
-    }
+        var crystalValue = parseInt($(event.target).data().value);
+        playerScore = playerScore + crystalValue;
+        $("#playerScore").text(playerScore)
+        $("#gameScore").text(gameScore)
+
+        console.log(playerScore);
+        if (playerScore === gameScore) {
+            wins++
+            alert("you win");
+        }
+        else if (playerScore > gameScore) {
+            losses++
+            alert("You suck");
+            reset();
+
+        }
     })
-    
+
 
 })
 
